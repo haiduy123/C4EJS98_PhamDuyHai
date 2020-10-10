7ec8873178d3a33b0c9d0fab63d8ecd3189fb2dc
jkfejk okroekrokw wkojdo <h>
2020-10-10 10:46:04 +0700

    no

---
+++ session_4/app.js
@@ -0,0 +1,29 @@
+// const arr =[1,2,3,5,7];
+// //map()
+// console.log(arr);
+// //cai index co the dat ten tuy y
+// const arr2= arr.map(function(item,index){
+//     return item+1;
+// })
+// //filter()---- lọc
+// const arr3=arr.filter(function(item,index){
+// return item>2
+// })
+// console.log(arr3);
+// let person=['Nam',18,'HaNoi',25,false];
+// console.log(person)
+// // ICRUD
+// INIT 
+// CREAT 
+// READ 
+// UPDATE 
+// DELETE 
+// let person={
+//     name: 'Nam'
+// };
+// console.log(person);
+let person={
+    name: 'Nam',
+    age: '18',
+};
+console.log(person);
---
+++ session_4/baitap.js
@@ -0,0 +1,73 @@
+        //BAI 1
+// let movie={
+//     title: 'The dark night rises',
+//     year: '2012',
+//     rate: '8.4'
+// };
+// console.log(movie);
+        //INIT
+// let movie={
+//     name: 'Spiderman',
+//     year: '2020'
+// };
+// console.log(movie);
+// console.log(movie.name);
+         //READ
+ let movie={
+     name: 'Spiderman',
+     year: '2020',
+     rate: '8.4'
+ };
+        // console.log(movie.name);
+        // console.log(movie['name']);
+        // console.log(movie.director);
+//  while(true){
+//          let input=prompt('What you want to know');
+//         if (input==='name'){
+//                 console.log(movie.name)
+//         }else if (input==='year'){
+//                 console.log(movie.year)
+//         }else{
+//                 console.log('FALSE')
+//         }
+        // UPDATE
+// console.log(movie['rate']);
+// movie['rate']=8.7;
+// console.log(movie['rate']);
+// movie['rate']=0.7;
+// console.log(movie['rate']);
+// let input=prompt('What you want to update');
+// let user=prompt('What is the update');
+// if (input==='name'){
+//         movie.name=user;
+//         console.log (movie)
+//         // console.log(movie.year)
+//         // console.log(movie.rate)
+// }else if (input==='year'){
+//         console.log(movie['name']);
+//         console.log (`${user}`);
+//         console.log(movie.rate);
+// }else if(input==='rate'){
+//         console.log(movie.name);
+//         console.log(movie.year);
+//         console.log (`${user}`);
+// }else {
+//         console.log('FALSE')    
+// }
+// let x=prompt('What you want to update');
+// if(x == null || x == undefined) {
+//         console.log(`${x} does not exit in our data, we will add new`)
+// }let a=prompt('Enter the new data');
+// console.log(movie)
+// console.log (`${x} :${a}`
+
+
+
+
+
+
+
+
+
+
+
