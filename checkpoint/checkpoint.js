93ecd3eef472a74816b53f37d44a37bc90246f6c
jkfejk okroekrokw wkojdo <h>
2020-10-13 21:20:03 +0700

    initial commit

---
+++ checkpoint/checkpoint.js
@@ -0,0 +1,10 @@
+//1
+let a =0;
+ let b = 1;
+ let number = Math.random() * (b- a)+a;
+ number = number.toFixed(2);
+ console.log(number);
+//2
+let arr=[2,5,6,9,10];
+var rand = number[Math.floor(Math.random()*arr.length)];
+console.log(rand);
