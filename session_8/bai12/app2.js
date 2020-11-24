e36ca08792c9678a95ebd83a4927092155e3ea6f
jkfejk okroekrokw wkojdo <h>
2020-11-24 20:48:56 +0700

    intial commit

--- session_8/bai12/app2.js
+++ session_8/bai12/app2.js
@@ -1,17 +1,35 @@
-var a=['Backpack','MiBand watch','Ring']
+let a=['Backpack','MiBand watch','Ring'];
 console.log(a);
-let x=document.getElementById("main-form");
-x.onsubmit=function(event){
-event.preventDefault();
-let a=todo.value;
-let li=document.createElement("li");
-li.innerText=a;
-let btn=document.createElement("button");
-btn.innerText="Remove";
-li.appendChild(btn);
-btn.onclick=function(){
-    li.parentNode.removeChild(li);
+let btn=document.getElementById("btn");
+let input=document.getElementById("todo")
+let ul=document.getElementById("list");
+for(let i=0;i<3;i++){
+  let li=document.createElement("li");
+  li.innerText=a[i];
+  let del=document.createElement("button");
+  li.appendChild(del);
+  ul.appendChild(li);
+  del.onclick=function(){
+    del.parentElement.remove();
+    a.splice(i,1);
+    console.log(a);
 }
-let ul=document.getElementById("list-1");
-ul.appendChild(li);
 }
+for(let i=0;i<3;i++){
+  btn.onclick=function(){
+  let x=input.value;
+  a.push(x);
+  let li=document.createElement("li");
+  li.innerText=x;
+  x.innerText=a[i];
+  ul.appendChild(li);
+  console.log(a);
+  let del=document.createElement("button");
+  li.appendChild(del);
+  del.onclick=function(){
+    del.parentElement.remove();
+    a.splice(i+1,1);
+    console.log(a);
+  }
+}
+  }
